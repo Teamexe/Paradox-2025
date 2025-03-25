5cd1540f9b328a52b816fef57ea297472eb205e6
Mehaksharmaa <156573871+Mehaksharmaa@users.noreply.github.com>
2025-03-25 23:40:32 +0530

    added front screen

--- client app/android/app/src/main/kotlin/com/example/paradox_2025/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.paradox_2025
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- paradox_frontend/lib/main.dart
+++ paradox_frontend/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/splash_screen.dart';
+import 'screens/splash_screen.dart' as splash;
 import 'screens/leaderboard_screen.dart';
 import 'screens/top_scorers_screen.dart';
 import 'screens/profile_screen.dart';
@@ -7,7 +7,7 @@ import 'screens/rules_screen.dart';
 import 'screens/quiz_screen.dart';
 
 void main() {
-  runApp(ParadoxApp());
+  runApp(const ParadoxApp());
 }
 
 class ParadoxApp extends StatelessWidget {
@@ -18,18 +18,19 @@ class ParadoxApp extends StatelessWidget {
     return MaterialApp(
       title: 'Paradox',
       theme: ThemeData(
-        primaryColor: Color(0xFFF75049),
+        primaryColor: const Color(0xFFF75049),
         scaffoldBackgroundColor: Colors.black,
         fontFamily: 'Orbitron',
       ),
-      home: SplashScreen(),
+      home: const splash.SplashScreen(),
       routes: {
-        '/leaderboard': (context) => LeaderboardScreen(),
-        '/top_scorers': (context) => TopScorersScreen(),
-        '/profile': (context) => ProfileScreen(),
-        '/rules': (context) => RulesScreen(),
-        '/quiz': (context) => QuizScreen(),
+        '/leaderboard': (context) => const LeaderboardScreen(),
+        '/top_scorers': (context) => const TopScorersScreen(),
+        '/profile': (context) => const ProfileScreen(),
+        '/rules': (context) => const RulesScreen(),
+        '/quiz': (context) => const QuizScreen(),
       },
     );
   }
 }
+
