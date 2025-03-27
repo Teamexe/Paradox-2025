fb1cadbface043c896efae07a0baf7b8b6687ac4
Mritunjaii <themritunjai@gmail.com>
2025-03-28 00:04:43 +0530

    Add leaderboard route to API for ranking functionality

--- server/src/routes/v1/index.js
+++ server/src/routes/v1/index.js
@@ -4,13 +4,15 @@ const authRoutes=require('./authRoutes')
 const HomeRoute=require('./homeRoutes')
 const QuestionRoute=require('./questionsRoutes')
 const AdminRoute=require('./adminRoutes')
+const leaderboard=require('./leaderboardRoutes')
 const app=express();
 
 
 app.use('/',HomeRoute)
 app.use('/auth', authRoutes);
-app.use('/question',QuestionRoute)
-app.use('/admin',AdminRoute)
+app.use('/question',QuestionRoute);
+app.use('/admin',AdminRoute);
+app.use('/rank',leaderboard);
 // app.use('/quiz', quizRoutes);
 // app.use('/leaderboard', leaderboardRoutes);
 
