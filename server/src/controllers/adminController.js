bcfe4a581822129bffe72720159504ff2ac08442
Mritunjaii <themritunjai@gmail.com>
2025-04-13 12:24:28 +0530

    Implement changeLevel functionality in AdminController and AdminService

--- server/src/controllers/adminController.js
+++ server/src/controllers/adminController.js
@@ -25,9 +25,28 @@ async function signIn(req,res) {
     }
 }
 
-
+async function changeLevel(req,res) {
+    try {
+        const data={
+            Ques:req.body.currQues,
+            Lvl:req.body.currLvl,
+            TopNumUser:req.body.TopNumUser
+        };
+        const user=await AdminService.changeLevel(data);
+        if(user){
+            SuccessResponse.data=user;
+            SuccessResponse.message="User level changed successfully";
+            return res.status(StatusCodes.OK).json(SuccessResponse);
+        }
+    } catch (error) {
+        console.log(error);
+        ErrorResponse.error=error;
+        return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+    }
+}
 
 
 module.exports={
-    signIn
+    signIn,
+    changeLevel
 }
--- server/src/routes/v1/adminRoutes.js
+++ server/src/routes/v1/adminRoutes.js
@@ -5,5 +5,6 @@ const router=express.Router();
 
 
 router.post('/signIn',AdminController.signIn);
+router.post('/changeLevel',AdminController.changeLevel);
 
 module.exports=router;
